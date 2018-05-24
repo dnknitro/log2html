b9a0812e96ade149341ea780e01afa03df0ae63d
dnknitro <vova64@gmail.com>
2018-05-23 22:06:16 -0500

    added .nuspec

---
+++ Tools/Content/GlobalTestSetup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace dnk.DynamicLog4netReport.Test
+{
+	[SetUpFixture]
+	public class GlobalTestSetup
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			HtmlReportAppender.Configure(
+				Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName, "Results"),
+				new ReportMetaData
+				{
+					ReportName = "Test Execution Report TEST",
+					ReportCategory = "Test",
+					ReportEnvironment = "DEV"
+				}
+			);
+		}
+	}
+}
