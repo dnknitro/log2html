4c8365d9891a4bd371367d72c8779c7dd015548f
dnknitro <vova64@gmail.com>
2018-05-23 21:58:20 -0500

    Added chart.png

--- DynamicLog4netReport/Properties/AssemblyInfo.cs
+++ DynamicLog4netReport/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicLog4netReport")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Html report generator based on log4net records" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "dnk / Volodymyr Shcherbyna" )]
 [assembly: AssemblyProduct("DynamicLog4netReport")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.0.0.*" )]
 [assembly: AssemblyFileVersion("1.0.0.0")]
