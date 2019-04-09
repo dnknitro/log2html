ff30c1c48f20bef6f3113ed5b5e256b39a3aed38
dnknitro <vova64@gmail.com>
2019-04-09 00:32:31 -0500

    Fixed build

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ if (!$NoInit) {
     elseif ($NuGetVersion -eq "latest") { & $NuGetFile update -Self }
 
     ExecSafe { & $NuGetFile restore $BuildProjectFile -SolutionDirectory $SolutionDirectory }
-    ExecSafe { & $NuGetFile install Nuke.MSBuildLocator -ExcludeVersion -OutputDirectory $TempDirectory -SolutionDirectory $SolutionDirectory }
+    ExecSafe { & $NuGetFile install Nuke.MSBuildLocator -Version 0.4.0 -ExcludeVersion -OutputDirectory $TempDirectory -SolutionDirectory $SolutionDirectory }
 }
 
 $MSBuildFile = & "$TempDirectory\Nuke.MSBuildLocator\tools\Nuke.MSBuildLocator.exe"
