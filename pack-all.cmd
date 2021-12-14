cd3e74e434248258d9621e5e78c2aecdc6294c5f
dnknitro <vova64@gmail.com>
2021-12-14 16:55:36 -0600

    nuget pack

--- pack-all.cmd
+++ pack-all.cmd
@@ -1,5 +1,7 @@
 @rem %1 should be full version, e.g. `pack-all.cmd 1.0.0.34`
 
+del .\pack\*.nupkg
+
 dotnet pack src\log2html --include-source --configuration Release --output .\pack\ -p:PackageVersion=%1
 
 dotnet pack src\log2html.support --include-source --configuration Release --output .\pack\ -p:PackageVersion=%1
