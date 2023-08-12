a146bc0da6dafaa059e7ee95dca453b67252394b
dnknitro <vova64@gmail.com>
2023-08-11 23:08:41 -0500

    Added scrollbox and syntax highlighting

--- src/log2html/Extensions.cs
+++ src/log2html/Extensions.cs
@@ -2,12 +2,21 @@
 
 public static class Extensions
 {
-	public static string NormalizeTestCaseName(this ITestCaseName testCaseNameProvider, string customTestCaseName)
-	{
-		var testCaseName = !string.IsNullOrWhiteSpace(customTestCaseName)
-			? ReportContext.Current.TestCaseName
-			: testCaseNameProvider?.GetName() ?? "Test Case Name not set";
-		if (testCaseName?.Contains("AdhocContext.AdhocTestMethod") == true) testCaseName = "Main";
-		return testCaseName;
-	}
+    public static string NormalizeTestCaseName(this ITestCaseName testCaseNameProvider, string customTestCaseName)
+    {
+        var testCaseName = !string.IsNullOrWhiteSpace(customTestCaseName)
+            ? ReportContext.Current.TestCaseName
+            : testCaseNameProvider?.GetName() ?? "Test Case Name not set";
+        if (testCaseName?.Contains("AdhocContext.AdhocTestMethod") == true) testCaseName = "Main";
+        return testCaseName;
+    }
+
+    /// <summary>
+    /// Wraps code string with code tags for farther processing on UI using https://highlightjs.org/
+    /// </summary>
+    public static string AsCode(this string code, string language = null)
+    {
+        var languageClass = !string.IsNullOrWhiteSpace(language) ? $" class=\"language-{language}\"" : "";
+        return $"<pre><code {languageClass}>{code}</code></pre>";
+    }
 }
