ea48ab6ea74a5b154f188087cb20769207ebfb05
dnknitro <vova64@gmail.com>
2024-03-27 09:54:31 -0500

    updated base path for index.html assets

--- src/log2html-template/vite.config.ts
+++ src/log2html-template/vite.config.ts
@@ -5,17 +5,19 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()/*, viteSingleFile()*/],
-	// build: {
-	// 	rollupOptions: {
-	// 		external: ['react', 'react-dom'],
-	// 		output: {
-	// 			// Provide global variables to use in the UMD build
-	// 			// for externalized deps
-	// 			globals: {
-	// 				'react': 'React',
-	// 				'react-dom': 'ReactDOM',
-	// 			},
-	// 		},
-	// 	},
-	// },
+	base: 'https://dnknitro.github.io/log2html/',
+	build: {
+		outDir: '../../docs/'
+		// 	rollupOptions: {
+		// 		external: ['react', 'react-dom'],
+		// 		output: {
+		// 			// Provide global variables to use in the UMD build
+		// 			// for externalized deps
+		// 			globals: {
+		// 				'react': 'React',
+		// 				'react-dom': 'ReactDOM',
+		// 			},
+		// 		},
+		// 	},
+	},
 })
